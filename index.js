851efc96f5f41d6d3a66f4c429d9502c3dc6ebf9
JAFFAR NGAIRA <garsolinajaffar@gmail.com>
2023-04-11 17:31:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,62 +1,44 @@
-const toyCollection = document.querySelector("#toy-collection")
-const addBtn = document.querySelector("#new-toy-btn")
-const toyForm = document.querySelector(".container")
-let addToy = false
-
-// STEP 1: Fetch all the toys from the server and render them to the DOM
-function getToys() {
+document.addEventListener("DOMContentLoaded", () => {
+  // code to fetch toy data goes here
+});
+fetch("http://localhost:3000/toys")
+  .then((response) => response.json())
+  .then((toys) => {
+    // code to render toys goes here
+  });
   fetch("http://localhost:3000/toys")
-    .then(response => response.json())
-    .then(toys => {
-      toys.forEach(toy => {
-        // STEP 2: Create a new HTML element for each toy and add it to the toy-collection div
-      })
-    })
-}
+  .then((response) => response.json())
+  .then((toys) => {
+    toys.forEach((toy) => {
+      // create a div element for the toy
+      const toyDiv = document.createElement("div");
+      toyDiv.className = "card";
 
-// STEP 4: Create a new toy object and make a POST request to add it to the database
-function createToy(toyData) {
-  fetch("http://localhost:3000/toys", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify(toyData)
-  })
-    .then(response => response.json())
-    .then(toy => {
-      // If the POST request is successful, create a new card element for the new toy and add it to the toy-collection div
-    })
-}
+      // add the toy's name to the div
+      const name = document.createElement("h2");
+      name.textContent = toy.name;
+      toyDiv.appendChild(name);
 
-// STEP 5: Update the like count of the corresponding toy object and make a PATCH request to update the database
-function updateLikes(toyId, likes) {
-  fetch(`http://localhost:3000/toys/${toyId}`, {
-    method: "PATCH",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json"
-    },
-    body: JSON.stringify({ likes: likes })
-  })
-    .then(response => response.json())
-    .then(toy => {
-      // If the PATCH request is successful, update the like count in the corresponding card element
-    })
-}
+      // add the toy's image to the div
+      const image = document.createElement("img");
+      image.className = "toy-avatar";
+      image.src = toy.image;
+      toyDiv.appendChild(image);
 
-// Call the getToys() function to fetch and render the toys on page load
-getToys()
+      // add the toy's likes to the div
+      const likes = document.createElement("p");
+      likes.textContent = toy.likes + " Likes";
+      toyDiv.appendChild(likes);
 
-// Add event listeners to the "Add Toy" button and the form "Submit" button
-addBtn.addEventListener("click", () => {
-  // Show/hide the form when the button is clicked
-})
+      // add a like button to the div
+      const button = document.createElement("button");
+      button.className = "like-btn";
+      button.textContent = "Like ❤️";
+      button.dataset.id = toy.id;
+      toyDiv.appendChild(button);
 
-toyForm.addEventListener("submit", event => {
-  event.preventDefault()
-  const toyData = {
-    // Get the data from the form inputs
-  }
-  createToy(toyData
+      // add the toy div to the toy collection div
+      const toyCollection = document.getElementById("toy-collection");
+      toyCollection.appendChild(toyDiv);
+    });
+  });
